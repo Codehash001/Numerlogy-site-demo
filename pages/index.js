f68be3d750a33d4651b87c1a2947cf908081c95e
Codehash001 <codehashblockchain@gmail.com>
2023-03-12 17:48:28 +0530

    first commit

---
+++ pages/index.js
@@ -0,0 +1,31 @@
+import Head from "next/head";
+import Image from "next/image";
+import Footer from "../components/footer";
+import FAQ from "../components/FAQs";
+import Header from "../components/header";
+import FAQs from "../components/FAQs";
+import { Link } from "react-scroll";
+import { useEffect, useState } from "react";
+
+export default function Index() {
+
+const [ArrowClicked , setArrowClicked ] = useState(false)
+
+const ArrowClickedHnadler= () => {
+    setArrowClicked(!ArrowClicked);
+  };
+  return (
+    <>
+      <Head>
+        <title>Skelee-Mini-web01</title>
+        <meta name="Description" content="skelee-mini-web" />
+        <link rel="icon" href="/Logoicon.png" />
+      </Head>
+      
+        <div className="h-screen w-screen bg-bg1 fixed top-0 -z-10 bg-cover bg-no-repeat bg-center">
+
+        
+        </div>
+    </>
+  );
+}
